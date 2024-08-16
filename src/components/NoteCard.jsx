ce5cd1e17a717618630318a4df3f692af5728da5
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-08-16 15:01:04 -0300

    feat: add more styling to the card component

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import { useRef } from "react";
+import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
   let position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
   const body = JSON.parse(note.body);
 
+  const textAreaRef = useRef(null);
   return (
     <div
       className="card"
       style={{
         backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
       }}
     >
+      <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
+        <Trash />
+      </div>
+      <div className="card-body">
+        <textarea
+          ref={textAreaRef}
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+        ></textarea>
+      </div>
       {body}
     </div>
   );
