7a2d1797300e78cd4d79d63cb1557f3e756a70ff
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-08-16 13:31:34 -0300

    feat: add cards with dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 import NotesPage from "./pages/NotesPage";
 
-function App() {
+const App = () => {
   return (
     <div id="app">
       <NotesPage />
     </div>
   );
-}
+};
+
+export default App;
