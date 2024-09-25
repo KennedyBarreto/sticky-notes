2db7182d5accb940ba29d2f784423976c1779e46
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-09-25 17:06:07 -0300

    feat: add react-router-dom to app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import React from "react";
 import NotesPage from "./pages/NotesPage";
 import NoteProvider from "./context/NoteContext";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div id="app">
-      <NoteProvider>
-        <NotesPage />
-      </NoteProvider>
+      <BrowserRouter>
+        <NoteProvider>
+          <NotesPage />
+        </NoteProvider>
+      </BrowserRouter>
     </div>
   );
 };
