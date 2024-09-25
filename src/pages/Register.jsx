3525131a3f785b849e673cfc9c3c3686a157b133
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-09-25 17:05:53 -0300

    feat: changes to register page html structure

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,38 +1,59 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import "../index.css"; // Importing the shared CSS file
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div className="auth-container">
-      <h2>Register</h2>
-      <form className="auth-form">
-        <div className="input-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" placeholder="Enter your name" required />
-        </div>
-        <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter your email"
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Enter your password"
-            required
-          />
-        </div>
-        <button type="submit" className="auth-button">
-          Register
-        </button>
-      </form>
+    <div className="container">
+      <div className="login-register-container">
+        <form>
+          <div className="form-field-wrapper">
+            <label>Name:</label>
+            <input
+              required
+              type="text"
+              name="name"
+              placeholder="Enter name..."
+            />
+          </div>
+
+          <div className="form-field-wrapper">
+            <label>Email:</label>
+            <input
+              required
+              type="email"
+              name="email"
+              placeholder="Enter email..."
+            />
+          </div>
+
+          <div className="form-field-wrapper">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password1"
+              placeholder="Enter password..."
+            />
+          </div>
+
+          <div className="form-field-wrapper">
+            <label>Confirm Password:</label>
+            <input
+              type="password"
+              name="password2"
+              placeholder="Confirm password..."
+            />
+          </div>
+
+          <div className="form-field-wrapper">
+            <input type="submit" value="Register" className="btn" />
+          </div>
+        </form>
+
+        <p>
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
     </div>
   );
 };
