a2ec677f0cd75d219eecc29f1d256ca1dca8735c
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-09-30 15:55:18 -0300

    feat: add login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PrivateRoutes from "./utils/PrivateRoutes";
+import { AuthProvider } from "./utils/AuthContext";
 const App = () => {
   return (
     <div id="app">
       <BrowserRouter>
-        <NoteProvider>
-          <Routes>
-            <Route element={<PrivateRoutes />}>
-              <Route path="/" element={<NotesPage />} />
-            </Route>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </NoteProvider>
+        <AuthProvider>
+          <NoteProvider>
+            <Routes>
+              <Route element={<PrivateRoutes />}>
+                <Route path="/" element={<NotesPage />} />
+              </Route>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </NoteProvider>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,28 @@
 /* eslint-disable react/no-unescaped-entities */
-/* eslint-disable no-unused-vars */
-import React from "react";
+import { useState } from "react";
+import { useAuth } from "../utils/AuthContext";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const { login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      // Redirect to the main page after successful login
+      window.location.href = "/";
+    } catch (error) {
+      console.error("Login failed", error);
+      // Handle login error (e.g., show an error message to the user)
+    }
+  };
   return (
     <div className="container">
       <div className="login-register-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-field-wrapper">
             <label>Email:</label>
             <input
@@ -15,6 +30,8 @@ const Login = () => {
               type="email"
               name="email"
               placeholder="Enter email..."
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -24,6 +41,8 @@ const Login = () => {
               type="password"
               name="password"
               placeholder="Enter password..."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
