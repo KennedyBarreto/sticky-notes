0e9b5b688d6784e7a72a3009136f7de2f1280a52
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-10-02 14:43:50 -0300

    feat: add register functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,34 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { account } from "../appwrite/config";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    try {
+      await account.create(crypto.randomUUID(), email, password, name);
+      // await account.createEmailPasswordSession(email, password);
+      navigate("/");
+    } catch (error) {
+      console.error("Registration failed", error);
+      alert("Registration failed. Please try again.");
+    }
+  };
+
   return (
     <div className="container">
       <div className="login-register-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-field-wrapper">
             <label>Name:</label>
             <input
@@ -14,6 +36,8 @@ const Register = () => {
               type="text"
               name="name"
               placeholder="Enter name..."
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
 
@@ -24,24 +48,32 @@ const Register = () => {
               type="email"
               name="email"
               placeholder="Enter email..."
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
           <div className="form-field-wrapper">
             <label>Password:</label>
             <input
+              required
               type="password"
               name="password1"
               placeholder="Enter password..."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
           <div className="form-field-wrapper">
             <label>Confirm Password:</label>
             <input
+              required
               type="password"
               name="password2"
               placeholder="Confirm password..."
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </div>
 
